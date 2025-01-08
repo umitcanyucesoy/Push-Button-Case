492ff00b399e35200e1645e8caafe541c482e5c9
umitcanyucesoy <umitcanyucesoy@gmail.com>
2025-01-08 10:09:07 +0300

    Hand Animation + DOTween installed

---
+++ PushButton/Assets/Scripts/Hand/FingertipTrigger.cs
@@ -0,0 +1,11 @@
+using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Hand
+{
+    public class FingertipTrigger : MonoBehaviour
+    {
+        
+    }
+}
---
+++ PushButton/Assets/Scripts/Hand/HandAnimationTween.cs
@@ -0,0 +1,33 @@
+using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Hand
+{
+    public class HandAnimationTween : MonoBehaviour
+    {
+        [Header("Animation Settings")]
+        public float pressDuration = 0.5f; 
+        public float pressDistance = 0.5f; 
+        private Vector3 _startPosition;
+
+        private void Start()
+        {
+            _startPosition = transform.position;
+            HandPressAnimation().Forget();
+        }
+
+        private async UniTask HandPressAnimation()
+        {
+            while (true)
+            {
+                transform.DOMoveY(_startPosition.y - pressDistance, pressDuration / 2).SetEase(Ease.InOutSine);
+                await UniTask.Delay(TimeSpan.FromSeconds(pressDuration/2));
+                
+                transform.DOMoveY(_startPosition.y, pressDuration / 2).SetEase(Ease.InOutSine);
+                await UniTask.Delay(TimeSpan.FromSeconds(pressDuration/2));
+            }
+        }
+    }
+}
